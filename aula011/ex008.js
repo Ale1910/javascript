f1f60d4325674621e3463fa9be4242521080b1a8
Ale1910 <128859995+Ale1910@users.noreply.github.com>
2024-02-15 20:30:58 -0300

    Ex010

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 58
+console.log(`A veolidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre com cinto de segurança`)
