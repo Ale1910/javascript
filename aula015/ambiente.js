03880198446a5ac8959a0f014acbcf77f493cdee
Ale1910 <128859995+Ale1910@users.noreply.github.com>
2024-02-18 20:03:30 -0300

    Aula015

---
+++ aula015/ambiente.js
@@ -0,0 +1,6 @@
+let num =  [5, 8, 2, 9, 3]
+num.push(7)
+num.sort()
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O Vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
